6c5999cb31c72a3807e716d7560a880188ccadf3
alvarezmartin3639 <60674744+alvarezmartin3639@users.noreply.github.com>
2020-02-11 22:18:23 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -5,13 +5,12 @@ function mostrar() {
     edad = parseInt(document.getElementById("edad").value);
 
     estadoCivil = document.getElementById("estadoCivil").value;
-
+    //Si edad es mayor a 18 y que sea sea soltero
     if (edad > 18 && estadoCivil != "Soltero") {
 
         alert("es soltero y no es menor");
-
-
     }
 
-
 }//FIN DE LA FUNCIÓN
+
+
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+
+	numero=document.getElementById())
+
+	Math.rando
 
 }//FIN DE LA FUNCIÓN
