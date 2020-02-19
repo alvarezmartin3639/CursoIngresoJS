4dfa903b5e11fae62a17a7617f406805a90b5c63
alvarezmartin3639 <60674744+alvarezmartin3639@users.noreply.github.com>
2020-02-18 22:13:06 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,6 +1,9 @@
 function mostrar() {
 
-	/* como se escribe
+	/* como se escribe se usa el do while cuando queres ejecutar el while aunque sea una vez
+	el while se usa cuando es un bucle infinito
+	el for cuando es un "bucle" con un numero determinado, por ejemplo si queres que se repita 3 veces
+	
 	do {
 
 	} while();
