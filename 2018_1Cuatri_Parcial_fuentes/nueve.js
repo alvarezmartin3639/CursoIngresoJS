472f9e7d3319f243bf975a5b2d101a4241c1f9da
alvarezmartin3639 <60674744+alvarezmartin3639@users.noreply.github.com>
2020-03-03 22:23:55 -0300

    Add files via upload

--- 2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -2,36 +2,82 @@ function mostrar() {
     var marca;
     var peso;
     var temperatura;
-    var tempPar;
-    var validador
+    var seguir;
+    var contadorPares = 0;
+    var marcaPesada;
+    var maximoPeso;
+    var flag = 0;
+    var contadorCongelados = 0;
+    var promedioPeso;
+    var acumuladorPeso = 0;
+    var contador = 0;
+    var pesoMax;
+    var pesoMin;
+
+    do {
+        // pido los datos
+        marca = prompt("Ingrese una marca: ");
+
+        peso = parseInt(prompt("Ingrese un peso (1 y 100): "));
+        while (peso < 1 || peso > 100 || isNaN(peso)) {
+            peso = parseInt(prompt("Peso invalido. Ingrese un peso (1 y 100): "));
+        }
+
+        temperatura = parseInt(prompt("Ingrese un temperatura (-30 y 30): "));
+        while (temperatura < -30 || temperatura > 30 || isNaN(temperatura)) {
+            temperatura = parseInt(prompt("Temperatura invalido. Ingrese un temperatura (-30 y 30): "));
+        }
+
+        if (temperatura % 2 == 0) {
+            contadorPares++;
+        }
+
+        if ((flag == 0 || peso > maximoPeso) && temperatura > 0) {
+            maximoPeso = peso;
+            marcaPesada = marca;
+
+            flag = 1;
+        }
+
+        if (temperatura < 0) {
+            contadorCongelados++;
+        }
 
 
-    
-    
-    
-    do{}    
-   
-    peso = parseInt(prompt("ingrese un peso"));
-   
-    
+        if (contador == 0 || peso > pesoMax) {
+            pesoMax = peso;
+        }
+        if (contador == 0 || peso < pesoMin) {
+            pesoMin = peso;
+        }
 
-    while (peso <= 0 || peso >= 101 || isNaN(peso)) {
-        peso = parseInt(prompt("peso invalido, ingresar otro"));
+        acumuladorPeso = acumuladorPeso + peso;
+        contador++;
+
+        seguir = prompt("Quiere continuar?: ");
+    } while (seguir == 's');
+
+
+    promedioPeso = acumuladorPeso / contador;
+
+
+
+
+
+
+    document.write("a) La cantidad de temperaturas pares: " + contadorPares + "</br>");
+    if (flag == 0) {
+        document.write("b) La marca del producto más pesado no congelado: No se ingresaron productos no congelados</br>");
 
     }
+    else {
 
-    temperatura = parseInt(prompt("ingrese una temperatura"));
-
-
-    while (temperatura <= -31 || temperatura >= 31 || isNaN(temperatura)) {
-        temperatura=parseInt(prompt("temperatura invalida, ingresar otra"))
-
+        document.write("b) La marca del producto más pesado no congelado: " + marcaPesada + " Peso: " + maximoPeso + "</br>");
     }
-
-    while (validador!='s') {
-        
-    }
-
-
+    document.write("c) La cantidad de productos que se conservan a menos de 0 grados: " + contadorCongelados + "</br>");
+    document.write("d) El promedio del peso de todos los productos: " + promedioPeso + "</br>");
+    document.write("e) Maximo: " + pesoMax + "</br>");
+    document.write("e) Minimo: " + pesoMin + "</br>");
 
 }
+//en el examen hay que validar != coca cola como si fuera el ejercicio de sexo f o m
